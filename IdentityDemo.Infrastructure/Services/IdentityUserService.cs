bd91fd9f984e08c5adf898edf2b53ef8f4ade4aa
Clive <Cliveswe@users.noreply.github.com>
2025-06-09 11:13:59 +0200

    Enhance IdentityUserService for role management
    - Updated IdentityUserService constructor to include RoleManager.
    - Added roleName constant for "Administrator" in CreateUserAsync.
    - Implemented logic to check and create the Administrator role based on user email.
    - Refactored method signatures to use braces for improved readability.
    - Maintained functionality of SignInAsync and SignOutAsync methods while ensuring consistent formatting.

--- IdentityDemo.Infrastructure/Services/IdentityUserService.cs
+++ IdentityDemo.Infrastructure/Services/IdentityUserService.cs
@@ -2,38 +2,44 @@
 using IdentityDemo.Application.Users;
 using IdentityDemo.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IdentityDemo.Infrastructure.Services;
 public class IdentityUserService(
     UserManager<ApplicationUser> userManager,
-    SignInManager<ApplicationUser> signInManager) : IIdentityUserService
+    SignInManager<ApplicationUser> signInManager,
+    RoleManager<IdentityRole> roleManager) : IIdentityUserService
 {
-    public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password)
-    {
-        var result = await userManager.CreateAsync(new ApplicationUser
-        {
+    public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password) {
+        const string roleName = "Administrator";
+
+
+        var result = await userManager.CreateAsync(new ApplicationUser {
             UserName = user.Email,
             Email = user.Email,
             FirstName = user.FirstName,
             LastName = user.LastName
         }, password);
 
+        if(user.Email.Contains("admin")) {
+
+            if(!await roleManager.RoleExistsAsync(roleName)) {
+                // Skapa en ny roll
+                var resultRole = await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+
+            // Lägg till en användare till en roll
+            //await userManager.AddToRoleAsync(resultRole, roleName);
+
+        }
         return new UserResultDto(result.Errors.FirstOrDefault()?.Description);
     }
 
-    public async Task<UserResultDto> SignInAsync(string email, string password)
-    {
+    public async Task<UserResultDto> SignInAsync(string email, string password) {
         var result = await signInManager.PasswordSignInAsync(email, password, false, false);
         return new UserResultDto(result.Succeeded ? null : "Invalid user credentials");
     }
 
-    public async Task SignOutAsync()
-    {
+    public async Task SignOutAsync() {
         await signInManager.SignOutAsync();
     }
 }

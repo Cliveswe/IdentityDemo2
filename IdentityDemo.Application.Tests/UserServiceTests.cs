2fd0afd98c12619739709dae59847366e85ab065
Peter Ahlgren <ahlgren1234@gmail.com>
2025-06-10 14:20:09 +0200

    Add USer test create

--- IdentityDemo.Application.Tests/UserServiceTests.cs
+++ IdentityDemo.Application.Tests/UserServiceTests.cs
@@ -1,11 +1,29 @@
-﻿namespace IdentityDemo.Application.Tests
+﻿using IdentityDemo.Application.Dtos;
+using IdentityDemo.Application.Users;
+using Moq;
+
+namespace IdentityDemo.Application.Tests
 {
     public class UserServiceTests
     {
         [Fact]
-        public void Test1()
+        public async Task CreateUserAsync_WithDtoAndPassword()
         {
+            // Arrange
+            var mockIdentityUserService = new Mock<IIdentityUserService>();
+            var userService = new UserService(mockIdentityUserService.Object);
+            var user = new UserProfileDto("John@mail.com", "John", "Doe");
+            var userResult = new UserResultDto(null);
+            mockIdentityUserService.Setup(s => s.CreateUserAsync(user, "Password_123")).ReturnsAsync(userResult);
 
+            // Act
+            var result = await userService.CreateUserAsync(user, "Password_123");
+
+            // Assert
+            Assert.Equal(userResult, result);
+            mockIdentityUserService.Verify(s => s.CreateUserAsync(user, "Password_123"), Times.Once);
         }
+
+
     }
 }

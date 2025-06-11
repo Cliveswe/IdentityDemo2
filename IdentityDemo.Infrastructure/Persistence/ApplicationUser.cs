1965d2b4e481661ab48b0981619d0c8370dd3f45
davarng <113583854+davarng@users.noreply.github.com>
2025-06-11 10:09:38 +0200

    adding annotations

--- IdentityDemo.Infrastructure/Persistence/ApplicationUser.cs
+++ IdentityDemo.Infrastructure/Persistence/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ using System.Threading.Tasks;
 namespace IdentityDemo.Infrastructure.Persistence;
 public class ApplicationUser : IdentityUser
 {
+    [MaxLength(30)]
     public string FirstName { get; set; } = null!;
+    [MaxLength(30)]
     public string LastName { get; set; } = null!;
 }
 
--- IdentityDemo.Web/Views/Account/RegisterVM.cs
+++ IdentityDemo.Web/Views/Account/RegisterVM.cs
@@ -5,23 +5,28 @@ namespace IdentityDemo.Web.Views.Account
     public class RegisterVM
     {
         [Required]
+        [Length(1, 30)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; } = null!;
 
         [Required]
+        [Length(1, 30)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; } = null!;
 
         [Required]
         [EmailAddress]
+        [Length(6, 150)]
         [Display(Name = "E-Mail")]
         public string Email { get; set; } = null!;
 
         [Required]
+        [Length(6, 150)]
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
 
         [Required]
+        [Length(6, 150)]
         [DataType(DataType.Password)]
         [Display(Name = "Repeat password")]
         [Compare(nameof(Password))]

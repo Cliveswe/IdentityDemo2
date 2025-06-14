8f22a1736a83805e794095964709ff7b52097ef0
Clive <Cliveswe@users.noreply.github.com>
2025-06-14 09:19:08 +0200

    Added Http and server error features including the controller and views.

---
+++ IdentityDemo.Web/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDemo.Web.Controllers;
+public class ErrorController : Controller
+{
+    [HttpGet("error/exception")]
+    public IActionResult ServerError() {
+        return View();
+    }
+
+    [HttpGet("error/http/{statusCode}")]
+    public IActionResult HttpError(int statusCode) {
+        return View(statusCode);
+    }
+
+
+
+    //[HttpGet("error/{statusCode:int}")]
+    //public IActionResult Error(int statusCode) {
+    //    return View("Error", statusCode);
+    //}
+
+}
--- IdentityDemo.Web/Program.cs
+++ IdentityDemo.Web/Program.cs
@@ -5,14 +5,12 @@ using IdentityDemo.Infrastructure.Persistence;
 using IdentityDemo.Infrastructure.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace IdentityDemo.Web;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
+    public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
         builder.Services.AddTransient<IUserService, UserService>();
@@ -26,8 +24,7 @@ public class Program
             o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         // Identity: Registera identity-klasserna och vilken DbContext som ska användas
-        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-        {
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
             // Här kan vi (om vi vill) ange inställningar för t.ex. lösenord
             // (ofta struntar man i detta och kör på default-värdena)
             options.Password.RequiredLength = 6;
@@ -35,8 +32,7 @@ public class Program
         }).AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
 
         // Identity: Hit ska icke inloggade användare skikas (om de besöker skyddade sidor)
-        builder.Services.ConfigureApplicationCookie(o =>
-        {
+        builder.Services.ConfigureApplicationCookie(o => {
             o.LoginPath = "/login";
             o.LogoutPath = "/logout";
         });
@@ -45,6 +41,12 @@ public class Program
 
         // Identity: Behörighet
         app.UseAuthorization();
+
+        if(!app.Environment.IsDevelopment())
+            app.UseExceptionHandler("/error/exception");
+
+        if(!app.Environment.IsDevelopment())
+            app.UseStatusCodePagesWithRedirects("/error/http/{0}");
 
         app.UseHttpsRedirection();
         app.MapControllers();
